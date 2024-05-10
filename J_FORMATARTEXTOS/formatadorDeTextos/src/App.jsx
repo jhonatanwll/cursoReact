55f9b9e7720b9d81480b0ca1e2bbd77ba824ee19
Jhonatan <jhonatanwcom@gmail.com>
2024-05-10 12:09:11 -0300

    GettingStarted

--- J_FORMATARTEXTOS/formatadorDeTextos/src/App.jsx
+++ J_FORMATARTEXTOS/formatadorDeTextos/src/App.jsx
@@ -3,16 +3,19 @@ import "./App.css";
 
 function App() {
   const [texto, setTexto] = useState("");
+  const [textoExibido, setTextoExibido] = useState("");
 
-  function handleClick(){
-    navigator.clipboard.writeText(texto)
-    alert("Copiado!")
+  function handleClick() {
+    const textoFormatado = texto.replace(/[^a-zA-Z0-9]/g, '')
+    setTexto(textoFormatado);
+    navigator.clipboard.writeText(textoFormatado);
+    setTextoExibido(textoFormatado);
+    alert(`"${textoFormatado}" Copiado!`);
   }
 
   return (
     <div>
       <h1>Formatador/Limpador de textos em React</h1>
-      <p>Cole o seu texto aqui:</p>
       <input
         type="text"
         value={texto}
@@ -20,7 +23,9 @@ function App() {
         placeholder="Digite algo..."
       />
       <button onClick={handleClick}>Copiar</button>
-      <p>Texto a ser copiado: "{texto}"</p>
+      <p>Texto Copiado: "{textoExibido}"</p>
+      <div>
+      </div>
     </div>
   );
 }
