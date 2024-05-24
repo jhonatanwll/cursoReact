4f4b20a82c36ffdf0784ca8c96961ddcd9182c1e
Jhonatan <jhonatanwcom@gmail.com>
2024-05-24 14:43:15 -0300

    Configurando o calculo do IMC

--- CALCULADORA_IMC/calculadoraDeIMC/src/App.jsx
+++ CALCULADORA_IMC/calculadoraDeIMC/src/App.jsx
@@ -1,13 +1,28 @@
-import ImcCalc from './components/ImcCalc';
+import { data } from "./data/data";
 
-import './App.css';
+import { useState } from "react";
+
+import ImcCalc from "./components/ImcCalc";
+import ImcTable from "./components/ImcTable";
+
+import "./App.css";
 
 function App() {
+
+  const calcImc = (e, height, weight) => {
+    e.preventDefault() 
+    console.log("Executou")
+  }
+  
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
+
   return (
-   <div className="App">
-    <ImcCalc /> 
-   </div>
-  )
+    <div className="container">
+      {!imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable />}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- CALCULADORA_IMC/calculadoraDeIMC/src/components/Button.jsx
+++ CALCULADORA_IMC/calculadoraDeIMC/src/components/Button.jsx
@@ -4,7 +4,6 @@ const Button = ({id, text, action}) => {
     const handleAction = (e) => {
         action(e)
         // Executa a função passada
-        //
     }
   return <button id={id} onClick={handleAction}>{text}</button>;
 };
