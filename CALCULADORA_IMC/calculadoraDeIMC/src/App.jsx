9c305a30167586f9988358bb5c5a6177efb1310f
Jhonatan <jhonatanwcom@gmail.com>
2024-05-27 15:05:14 -0300

    Funcionalidades da calculadora finalizadas

--- CALCULADORA_IMC/calculadoraDeIMC/src/App.jsx
+++ CALCULADORA_IMC/calculadoraDeIMC/src/App.jsx
@@ -13,21 +13,46 @@ function App() {
 
     if (!weight || !height) return;
 
-    const weightFloat = +weight.replace(",",".");
-    const heightFloat = +height.replace(",",".");
+    const weightFloat = +weight.replace(",", ".");
+    const heightFloat = +height.replace(",", ".");
 
     const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1);
 
     setImc(imcResult);
+
+    data.forEach((item) => {
+      if (imcResult >= item.min && imcResult <= item.max) setInfo(item.info);
+      setInfoClass(item.infoClass);
+    });
+
+    if (!info) return;
+  };
+
+  const resetCalc = (e) => {
+    e.preventDefault();
+
+    setImc("");
+    setInfo("");
+    setInfoClass("");
   };
 
   const [imc, setImc] = useState("");
   const [info, setInfo] = useState("");
   const [infoClass, setInfoClass] = useState("");
-  
+
   return (
     <div className="container">
-      {imc ? <ImcTable /> : <ImcCalc calcImc={calcImc} />}
+      {imc ? (
+        <ImcTable
+          data={data}
+          imc={imc}
+          info={info}
+          infoClass={infoClass}
+          resetCalc={resetCalc}
+        />
+      ) : (
+        <ImcCalc calcImc={calcImc} />
+      )}
     </div>
   );
 }
