e6cf05cb33b94d499c3b5349ebf102e1e1272967
Jhonatan <jhonatanwcom@gmail.com>
2024-06-27 14:32:07 -0300

    Alterando e enviando os dados do formulário

--- _APP_TELA_DE_LOGIN/src/Components/Login/Login.jsx
+++ _APP_TELA_DE_LOGIN/src/Components/Login/Login.jsx
@@ -1,30 +1,56 @@
 import { FaUser, FaLock } from "react-icons/fa";
 
+import { useState } from "react";
+
+import "./Login.css";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    alert("Enviando os dados")
+   
+  };
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Acesse o sistema</h1>
         <div>
-          <input type="email" placeholder="Email" />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            onChange={(e) => setUsername(e.target.value)}
+            value={username || ""}
+          />
           <FaUser className="icon" />
         </div>
         <div>
-          <input type="password" placeholder="Senha" />
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <FaLock className="icon" />
         </div>
 
         <div className="recall-forget">
-            <label>
-                <input type="checkbox" />
-                Lembre de mim 
-            </label>
-            <a href="#">Esqueceu a senha?</a>
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="#">Esqueceu a senha?</a>
         </div>
 
         <button>Entrar</button>
         <div className="sigup-link">
-            <p>Não tem uma conta? <a href="#">Registre aqui</a></p>
+          <p>
+            Não tem uma conta? <a href="#">Registre aqui</a>
+          </p>
         </div>
       </form>
     </div>
