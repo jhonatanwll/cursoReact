e54b14a34158c082863ebe3d49881fc67e5a8eed
Jhonatan <jhonatanwcom@gmail.com>
2024-08-26 14:51:36 -0300

    multiformsteps novo componente

--- 6_FORMULARIO_MULTISTEP_REACT/multistep_form_react/src/App.jsx
+++ 6_FORMULARIO_MULTISTEP_REACT/multistep_form_react/src/App.jsx
@@ -2,8 +2,17 @@ import "./App.css";
 
 // Components
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import UserForm from "../components/UserForm";
+import ReviewForm from "../components/ReviewForm";
+import Thanks from "../components/Thanks";
+
+//Hooks
+import { useForm } from "../hooks/useForm";
 
 function App() {
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
+
+  const {currentStep, currentComponent} = useForm(formComponents)
   return (
     <div className="app">
       <div className="header">
@@ -16,7 +25,7 @@ function App() {
       <div className="form-container">
         <p>etapas</p>
         <form>
-          <div className="inputs-container"></div>
+          <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
             <button type="button">
               <GrFormPrevious />
