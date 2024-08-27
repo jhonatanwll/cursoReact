394bbc3947133e457939d6b1e097df4d3bdf96e6
Jhonatan <jhonatanwcom@gmail.com>
2024-08-27 15:06:39 -0300

    Multistep troca de passos automatizada com botões e hook

--- 6_FORMULARIO_MULTISTEP_REACT/multistep_form_react/src/App.jsx
+++ 6_FORMULARIO_MULTISTEP_REACT/multistep_form_react/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 
 // Components
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { FiSend } from "react-icons/fi";
 import UserForm from "../components/UserForm";
 import ReviewForm from "../components/ReviewForm";
 import Thanks from "../components/Thanks";
@@ -12,7 +13,8 @@ import { useForm } from "../hooks/useForm";
 function App() {
   const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
 
-  const {currentStep, currentComponent} = useForm(formComponents)
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } =
+    useForm(formComponents);
   return (
     <div className="app">
       <div className="header">
@@ -24,17 +26,26 @@ function App() {
       </div>
       <div className="form-container">
         <p>etapas</p>
-        <form>
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-            <button type="button">
+            {!isFirstStep && (
+              <button type="button" onClick={() => changeStep(currentStep - 1)}>
               <GrFormPrevious />
               <span>Voltar</span>
             </button>
-            <button type="submit">
-              <span>Avançar</span>
-              <GrFormNext />
-            </button>
+            )}
+            {!isLastStep ? (
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            ) : (
+              <button type="button">
+                <span>Enviar</span>
+                <FiSend />
+              </button>
+            )}
           </div>
         </form>
       </div>
