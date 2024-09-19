398a4093ca47fe994db69f2a4fcd06987d8379b2
Jhonatan <jhonatanwcom@gmail.com>
2024-09-19 15:16:30 -0300

    recebendo dados do json e colocando na página

--- 7_HTTP_REACT/http_react/src/App.jsx
+++ 7_HTTP_REACT/http_react/src/App.jsx
@@ -1,11 +1,33 @@
-import './App.css'
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
+
+import "./App.css";
 
 function App() {
- return (
-  <div className="App">
-    <h1>HTTP em React</h1>
-  </div>
- )
+  // 1 - resgatando dados
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    }
+    getData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>HTTP em React</h1>
+      {/* 1 - resgate de daos */}
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R${product.price}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
