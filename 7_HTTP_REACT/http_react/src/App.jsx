9b9199c17fe2e7b39d3a82ac18f87fe036eb5abb
Jhonatan <jhonatanwcom@gmail.com>
2024-09-20 15:08:56 -0300

    Administrando o banco de dados

--- 7_HTTP_REACT/http_react/src/App.jsx
+++ 7_HTTP_REACT/http_react/src/App.jsx
@@ -17,15 +17,59 @@ function App() {
     getData();
   }, []);
 
+  // 2 - envio de dados
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleSubmit =  async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    })
+  };
+
   return (
     <div className="App">
       <h1>HTTP em React</h1>
       {/* 1 - resgate de daos */}
       <ul>
         {products.map((product) => (
-          <li key={product.id}>{product.name} - R${product.price}</li>
+          <li key={product.id}>
+            {product.name} - R${product.price}
+          </li>
         ))}
       </ul>
+      {/* 2 - enviando os dados */}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Nome</span>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            <span>Preço</span>
+            <input
+              type="text"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
     </div>
   );
 }
