b61d74a31c551ef65ee60d83387d2639206b6df3
Jhonatan <jhonatanwcom@gmail.com>
2024-11-13 11:46:07 -0300

    Novo projeto "Quiz App"

---
+++ 9_QUIZ/quiz/src/App.jsx
@@ -0,0 +1,15 @@
+// react, components, stactics
+import Welcome from "./components/Welcome";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div class="App">
+      <h1>Novo Projeto</h1>
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
