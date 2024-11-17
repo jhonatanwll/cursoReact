437d01dcd57e0260d1d3e0a6e2b9e236f1db8813
Jhonatan <jhonatanwcom@gmail.com>
2024-11-17 17:17:07 -0300

    Estados do jogo Quiz

--- 9_QUIZ/quiz/src/App.jsx
+++ 9_QUIZ/quiz/src/App.jsx
@@ -1,13 +1,20 @@
 // react, components, stactics
+import { useContext } from "react";
+import { QuizContext } from "./context/quiz";
+
 import Welcome from "./components/Welcome";
+import Question from "./components/Question";
 
 import "./App.css";
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div class="App">
-      <h1>Novo Projeto</h1>
-      <Welcome />
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
     </div>
   );
 }
