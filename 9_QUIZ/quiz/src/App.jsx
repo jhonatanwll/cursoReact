ae85341dcacdeb3dc1d06e224610be92ce33c1d9
Jhonatan <jhonatanwcom@gmail.com>
2024-11-23 14:38:40 -0300

    novo componente PickCategory

--- 9_QUIZ/quiz/src/App.jsx
+++ 9_QUIZ/quiz/src/App.jsx
@@ -5,6 +5,7 @@ import { QuizContext } from "./context/quiz";
 import Welcome from "./components/Welcome";
 import Question from "./components/Question";
 import GameOver from "./components/GameOver";
+import PickCategory from "./components/PickCategory";
 
 import "./App.css";
 
@@ -20,6 +21,7 @@ function App() {
     <div class="App">
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
       {quizState.gameStage === "Playing" && <Question />}
       {quizState.gameStage === "End" && <GameOver />}
     </div>
