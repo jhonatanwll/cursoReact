d25a4120e27b9498dd14bde040c483e7219bab0d
Jhonatan <jhonatanwcom@gmail.com>
2024-12-08 18:26:51 -0300

    Novo projeto Context API

---
+++ 11_CONTEXT_API/context/src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+import Home from "./routes/Home.jsx";
+import { Contact } from "./routes/Contact.jsx";
+
+// 2 - criando o provider
+import { CounterContextProvider } from "./context/CounterContext.jsx";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <CounterContextProvider>
+      <RouterProvider router={router} />
+    </CounterContextProvider>
+  </StrictMode>
+);
---
+++ 11_CONTEXT_API/context/src/routes/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Contact = () => {
+  return (
+    <div>
+        <h1>Página de contato</h1>
+    </div>
+  )
+}
