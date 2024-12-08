7c9473afd72a88b2b7b401237add003a674fcb4d
Jhonatan <jhonatanwcom@gmail.com>
2024-12-08 18:43:18 -0300

    Context e Provider finalizados.

--- 11_CONTEXT_API/context/src/routes/Contact.jsx
+++ 11_CONTEXT_API/context/src/routes/Contact.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+// 3 -  alterando o valor do contexto
+import { useContext } from "react";
+import { CounterContext } from "../context/CounterContext.jsx";
 
 export const Contact = () => {
+  const { counter } = useContext(CounterContext);
+
   return (
     <div>
-        <h1>Página de contato</h1>
+      <h1>Página de contato</h1>
+      <p>Valor do contador: {counter}</p>
     </div>
-  )
-}
+  );
+};
--- 11_CONTEXT_API/context/src/routes/Home.jsx
+++ 11_CONTEXT_API/context/src/routes/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+// 3 -  alterando o valor do contexto
+import { useContext } from "react";
+import ChangeCounter from "../components/ChangeCounter.jsx";
+
+import { CounterContext } from "../context/CounterContext.jsx";
 
 const Home = () => {
+  const { counter } = useContext(CounterContext);
+
   return (
     <div>
       <h1>Home</h1>
+      <p>Valor do contador: {counter}</p>
+      <ChangeCounter />
     </div>
   );
 };
