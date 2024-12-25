c54b7678a05a19b55d5f78bc1562ca85efe49fec
Jhonatan <jhonatanwcom@gmail.com>
2024-12-25 14:54:46 -0300

    Festas sendo criadas e registradas no banco de dados, mensagens do Toastify configuradas com hook e implementadas.

--- 12_PARTY_TIME_WEB/frontend/src/App.jsx
+++ 12_PARTY_TIME_WEB/frontend/src/App.jsx
@@ -3,11 +3,16 @@ import { Outlet } from "react-router-dom";
 // Components
 import Navbar from "./components/Navbar";
 
+import { ToastContainer } from "react-toastify";
+
+// Styles
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div>
+      <ToastContainer />
       <Navbar />
       <Outlet />
     </div>
